bdb3b462ffc44b5374ef083b1f5b0fdbed69bcf2
raulrrdwdcefcerfvsasn <raul@xix6s.comfgvfevcf>
2022-05-22 21:36:59 -0400

    Ready to dev model

--- PCF/checklist/checklistv1/index.ts
+++ PCF/checklist/checklistv1/index.ts
@@ -6,26 +6,24 @@ import * as ReactDOM from 'react-dom';
 //FLUENTUI
 import { initializeIcons } from '@fluentui/react/lib/Icons';
 
-
 //CUSTOM
-import { CheckListApp, ICheckListProps } from './components/CheckListApp';
-
+//import { CheckListApp, ICheckListProps } from './components/CheckListApp';
+//import { runInThisContext } from "vm";
 
 type DataSet = ComponentFramework.PropertyTypes.DataSet;
 initializeIcons(undefined, { disableWarnings: true });
 
 export class checklistv1 implements ComponentFramework.StandardControl<IInputs, IOutputs> {
-    entity: string = "";
     private _notifyOutputChanged: () => void;
+    private _context: ComponentFramework.Context<IInputs>;
     private _container: HTMLDivElement;
-    private _appprops: ICheckListProps;
+    private _formContainer: HTMLElement;
+    //private _appprops: ICheckListProps;
 
     /**
      * Empty constructor.
      */
-    constructor()
-    {
-
+    constructor() {
     }
 
     /**
@@ -36,23 +34,25 @@ export class checklistv1 implements ComponentFramework.StandardControl<IInputs, 
      * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
      * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
      */
-    public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement): void
-    {
+    public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container: HTMLDivElement): void {
         console.log('INIT-------------------------------------');
         console.log(context);
+        this._notifyOutputChanged = notifyOutputChanged;
+        this._context = context;
         this._container = container;
-        //context.mode.trackContainerResize(true);
-
+        context.mode.trackContainerResize(true);
+        //this._formContainer = new HTMLDivElement();
+        //this._formContainer.innerHTML = '';
+        //this.renderCheckList(context);
     }
-
 
     /**
      * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
      * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
      */
-    public updateView(context: ComponentFramework.Context<IInputs>): void
-    {
+    public async updateView(context: ComponentFramework.Context<IInputs>): Promise<void> {
         console.log('updateView---------------------');
+
         //this._appprops.dataset = context.parameters.sampleDataSet;
         ////Is this only a Template? **CHANGE THE HARD CODE
         //this._appprops.isTemplate = false;
@@ -63,19 +63,25 @@ export class checklistv1 implements ComponentFramework.StandardControl<IInputs, 
         //    this._container
         //);
 
-        this.renderCheckList(context);
+        const sections = this._context.parameters.sampleDataSet.records;
+
+        const questionsResponse = await this.retrieveQuestions(sections);
+        //console.log(questionsResponse);
+        const optionsResponse = await this.retrieveQuestionOptions(questionsResponse);
+        //console.log(optionsResponse);
+
+
+        this.renderCheckList(questionsResponse, optionsResponse);
     }
 
     /**
      * It is called by the framework prior to a control receiving new data.
      * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
      */
-    public getOutputs(): IOutputs
-    {
+    public getOutputs(): IOutputs {
         console.log('INDEX - getOutputs------------------------');
         console.log(this);
         return {
-
         };
     }
 
@@ -83,43 +89,154 @@ export class checklistv1 implements ComponentFramework.StandardControl<IInputs, 
      * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
      * i.e. cancelling any pending remote calls, removing listeners, etc.
      */
-    public destroy(): void
-    {
+    public destroy(): void {
         // Add code to cleanup control if necessary
         ReactDOM.unmountComponentAtNode(this._container);
     }
 
-    private renderCheckList(context: ComponentFramework.Context<IInputs>) {
-        console.log('renderCheckList---------------');
+    private async retrieveQuestions(sections: any) {
+        console.log('retrieveQuestions-------------------');
+        //console.log(sections);
+        let requestArray = [] as any;
+        for (const key in sections) {
 
-        let checkList = '';
-        let sections = context.parameters.sampleDataSet.records;
+            let request = '?$filter=_xix_checklistsection_value eq ' + key;
+
+            requestArray.push(this._context.webAPI.retrieveMultipleRecords("xix_question", request));
+        }
+
+        const response = await Promise.all(requestArray);
+        //console.log(response);
+
         
+        if (response) {
+            return response;
+        }
+        return null;
+    }
 
-        
-        //context.parameters.sampleDataSet.records['6ebba16f-9ecf-ec11-a7b5-6045bd0116db']._record.fields.createdby
+    private async retrieveQuestionOptions(questions: any) {
+        console.log('retrieveQuestionOptions-------------------');
+        //console.log(questions);
 
-        context.webAPI.retrieveMultipleRecords('xix_question', '?$filter=_xix_checklistsection_value eq d662adba-a9cd-ec11-a7b5-6045bd0066f1').then(
-            function success(results) {
-                for (var i = 0; i < results.entities.length; i++) {
-                    var xix_questionid = results.entities[i]["xix_questionid"];
-                }
-            },
-            function (error) {
-                console.log(error);
+        let requestArray = [] as any;
+        for (var i = 0; i < questions.length; i++) {
+            let currentQuestionSection = questions[i];
+            for (var j = 0; j < currentQuestionSection.entities.length; j++) {
+                let request = '?$filter=_xix_question_value eq ' + currentQuestionSection.entities[j].xix_questionid;
+                requestArray.push(this._context.webAPI.retrieveMultipleRecords("xix_questionoption", request));
             }
-        );
 
-        let sectionhtml = `<div class="ms-Grid" dir="ltr">
-  <div class="ms-Grid-row">
-    <div class="ms-Grid-col ms-sm6 ms-md4 ms-lg2">A</div>
-    <div class="ms-Grid-col ms-sm6 ms-md8 ms-lg10">B</div>
-  </div>
-</div>`;
+        }
 
-        this._container.innerHTML = sectionhtml;
+        const response = await Promise.all(requestArray);
+        //console.log(response);
+        if (response) {
+
+            return response;
+        }
+        return null;
+    
+    }
+
+    private async renderCheckList(questions: any, options: any) {
+        console.log('renderCheckList---------------');
+        console.log(questions);
+        console.log(options);
+
+
+        let formHtml = '';
+        const sections = this._context.parameters.sampleDataSet.records;
+
+        for (const key in sections) {
+
+            let currentSection = sections[key] as any;
+            console.log(currentSection);
+            const sectionGuid = currentSection._record.identifier.id.guid;
+            const sectionTitle = currentSection._record.fields.xix_checklistsectiontitle.value;
+            const sectionRequired = currentSection._record.fields.xix_requiredsection.label; //YES/NO
+            const sectionText = currentSection._record.fields.xix_sectionhinttext?.value;
+            const sectionAddText = currentSection._record.fields.xix_sectionadditionaltext?.value;
+            const associatedQuestions = questions.filter((question: any) => {
+                if (question.entities[0]._xix_checklistsection_value === sectionGuid) return question;
+            });
+            console.log(associatedQuestions);
+
+            formHtml += `<h1>` + sectionTitle + `</h1>`;
+
+            for (var i = 0; i < associatedQuestions.length; i++) {
+                if (associatedQuestions[i]) {                   
+                    console.log(associatedQuestions[i]);
+                    associatedQuestions[i].entities.forEach((question: any) => {
+                        console.log(question);
+
+                        //Question headers
+                        formHtml += `<h1>` + question.xix_questiontitle + `</h1>`;
+                        /*formHtml += `<h3>` + question.xix_questiontitle + `</h3>`;*/
+
+
+                        //create question component per question type
+                        //Radio
+                        if (question.xix_questiontype === 596810001) {
+                            //let choices = 
+                            formHtml += `<div class="form-check">`;
+
+                            options.forEach((option: any) => {
+                                console.log(option);
+
+                                formHtml += `<input type="radio" name="` + option.xix_optionvalue + `" id="` + option.xix_questionoptionid + `">
+              <label for="` + option.xix_optionvalue + `">
+                ` + option.xix_optionlabel + `
+              </label>`
+                            });
+
+                            formHtml += `</div>`;
+
+                        }
+
+                        //Drop-down
+                        if (question.xix_questiontype === 596810000) {
+                            //let choices = 
+                            formHtml += `<label for="options">Select...</label><select name="options" id="options">`;
+
+                            options.forEach((option: any) => {
+                                console.log(option);
+                                option.entities.forEach((optionPiece: any) => {
+                                    console.log(optionPiece);
+                                    formHtml += `<option value="` + optionPiece.xix_optionvalue + `">` + optionPiece.xix_optionvalue + `</option>`;
+                                });
+                                
+
+                                formHtml += `</select>`;
+                            });
+
+                            formHtml += `</div>`;
+
+                        }
+
+                        //Check if it has no antecent question or question option
+                        //if (question._xix_antecedentoption_value || question._xix_antecedentquestion_value) {
+                        //    console.log('ANTECEDENT record');
+                        //    //formHtml += `<h1>` + associatedQuestions[i].entities[i].xix_questiontitle + `</h1>`;
+                        //}
+
+
+                    });
+
+                   
+                    
+                }
+                
+            }
+            
+
+            
+        }
+        
+
+        this._container.innerHTML = formHtml;
+
 
     }
-    
 
 }
